df4c65d63178cb01fbe35e1ae445fa2916b75eae
Zhanarys <arsik0153@gmail.com@>
2023-01-12 09:04:48 +0600

    Start adaptive

--- main.js
+++ main.js
@@ -39,3 +39,16 @@ accordions.forEach((el) =>
         }
     })
 );
+
+// Mobile menu
+const open = document.querySelector("#open-menu");
+const close = document.querySelector("#close-menu");
+const menu = document.querySelector(".mobile-menu");
+
+open.addEventListener("click", () => {
+    menu.style.display = "block";
+});
+
+close.addEventListener("click", () => {
+    menu.style.display = "none";
+});
